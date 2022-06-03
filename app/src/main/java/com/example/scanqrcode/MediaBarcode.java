b3cc458f441cf77b1c83abac978c867728262d13
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2022-06-03 08:26:13 +0800

    Fix get data from barcode and qrcode 🐰

--- app/src/main/java/com/example/scanqrcode/MediaBarcode.java
+++ app/src/main/java/com/example/scanqrcode/MediaBarcode.java
@@ -1,16 +1,35 @@
 package com.example.scanqrcode;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.Result;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Queue;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MediaBarcode extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
-    private ZXingScannerView mScannerView;
+     ZXingScannerView mScannerView;
+     String kode = "";
+     String nama="";
+     String harga = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +43,73 @@ public class MediaBarcode extends AppCompatActivity implements ZXingScannerView.
     public void handleResult(Result result) {
 
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        getDataBarang(result.getText());
+
+
 
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+   // Method untuk memanggil data barang
+    
+    private void getDataBarang(String result){
+        String url="http://192.168.11.19/qrcode/cari_qrcode.php?kode="+result;
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.PUT,url,null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray jsonArray) {
+                        try {
+
+                            for(int i = 0; i < jsonArray.length(); i++) {
+
+
+                                JSONObject jsonobject = jsonArray.getJSONObject(i);
+
+                                kode = jsonobject.getString("kode");
+                                nama = jsonobject.getString("nama_barang");
+                                harga = jsonobject.getString("harga");
+
+                                AlertDialog alertDialog = new AlertDialog.Builder(MediaBarcode.this).create();
+                                alertDialog.setTitle("Hasil Scanning");
+                                alertDialog.setMessage("Kode Barcode : " + kode + "\nNama Barang : " + nama + "\nHarga Barang : " + harga);
+
+                                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.dismiss();
+                                                finish();
+                                            }
+                                        });
+                                alertDialog.show();
+                                Toast.makeText(MediaBarcode.this, "Barang ditemukan: "+nama, Toast.LENGTH_SHORT).show();
+
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+    }
+
 }
--- app/src/main/java/com/example/scanqrcode/ScanQrCode.java
+++ app/src/main/java/com/example/scanqrcode/ScanQrCode.java
@@ -2,13 +2,52 @@ package com.example.scanqrcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ScanQrCode extends AppCompatActivity {
+
+    Button scan;
+    FloatingActionButton fabScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_scan_qr_code);
+
+
+        sembunyiinNavbar();
+        init();
+
+        scan.setOnClickListener(view -> {
+            moveToScanQrCode();
+        });
+
+        fabScan.setOnClickListener(view -> {
+           moveToScanQrCode();
+        });
+
+
     }
+
+    private void sembunyiinNavbar() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    private void init() {
+        scan = findViewById(R.id.btn_scannow);
+        fabScan = findViewById(R.id.btn_scan);
+    }
+
+    private void moveToScanQrCode() {
+        startActivity(new Intent(getApplicationContext(), MediaBarcode.class));
+    }
+
 }
